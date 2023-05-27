f8e90e776566b72ee46b0f63e5fb4647403cfe15
yamamura <ayatsujisan2@gmail.com>
2023-05-27 13:38:19 +0900

    コミットします

--- about.php
+++ about.php
@@ -1,11 +1,140 @@
 <?php get_header(); ?>
 
 <!-- main -->
-<div class="about-main">
-    <div class="about-main-area">
-        <div class="abot-main-areaText">
-
+ <!-- about.php main -->
+ <div class="about-main">
+        <div class="about-main-area">
+            <div class="about-main-areaText">
+                <div class="top-about-title">
+                    <h1 class="tabout-tit">CONCEPT</h1>
+                    <span class="sub-tab-text">おにぎりやとは</span>
+                </div>
+            </div>
         </div>
     </div>
-</div>
+    <div class="link-area-top">
+        <div class="link-area-about">
+            <ul class="list-link-about">
+                <li class="list-alink">
+                    <a href="#" class="about-top-link">TOP</a>
+                </li>
+                <li class="about-top-link-sub">おにぎりやとは</li>
+            </ul>
+        </div>
+    </div>
+    <div class="about-concept-main">
+        <div class="about-main-flex">
+            <div class="about-concept">
+                <div class="about-concept-area">
+                    <div class="about-concept-detail">
+                        <h2 class="concept-textArea">
+                            <span class="costext">美味しさに</span> <br>
+                            <span class="costext">真心と温もりを。</span>
+                        </h2>
+                    </div>
+                </div>
+            </div>
+            <div class="about-image-area">
+                <div class="top-about-logoimage">
+                    <div class="tabut-image">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/onigiri_tenmusu.png" alt="サブ画像" class="about-subimage">
+                    </div>
+                </div>
+                <div class="top-about-mainimgArea">
+                    <div class="top-about-mainimg">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/images1.jpg" alt="メイン画像">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="about-detail-main">
+        <div class="about-detail-area">
+            <div class="about-detail">
+                <div class="about-detail-images">
+                    <div class="about-img1">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/images4.jpg" alt="画像1" class="about-img1">
+                    </div>
+                    <div class="about-img2">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/images8.jpg" alt="画像2" class="about-img2">
+                    </div>
+                    <div class="about-detail-textArea">
+                        <p class="about-detail-text">私達が日本のお米を食べることは、日本の環境保全や生物多様性にもつながります。
+                            食べ手がいてこそ、作り手や田んぼを維持することができ、田んぼは人の手が加わってこそ
+                            維持することができるのです。
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="profile-main">
+        <div class="profile-area-main">
+            <div class="profile-area-maincontent">
+                <h2 class="profile-texte">PROFILE</h2>
+                <span class="profile-textja">プロフィール</span>
+            </div>
+            <div class="profile-history-main">
+                <div class="profile-history-area">
+                    <div class="profile-history-image">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/human.jpg" alt="画像" class="profile-img">
+                    </div>
+                    <div class="profile-company-main">
+                        <div class="profile-company-area">
+                            <div class="profile-company-flex">
+                                <div class="profile-company-detail">
+                                    <h2 class="company-name">株式会社おにぎりや</h2>
+                                    <div class="profile-name">
+                                        <small class="profile-daihyo">代表取締役</small>
+                                        <span class="daihyo-name">山田太郎</span>
+                                    </div>
+                                </div>
+                                <div class="license-area">
+                                    <ul class="license-detail">
+                                        <li class="license-text">調理師</li>
+                                        <li class="license-text">食生活アドバイザー</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="history-main">
+                            <dl class="history-area">
+                                <dt class="history-day">1971.9</dt>
+                                <dd class="history-detail">静岡県浜松市生まれ</dd>
+                            </dl>
+                            <dl class="history-area">
+                                <dt class="history-day">1995.3</dt>
+                                <dd class="history-detail">東北工業大学卒業</dd>
+                            </dl>
+                            <dl class="history-area">
+                                <dt class="history-day">1995.4</dt>
+                                <dd class="history-detail">食品会社に入社 惣菜・加工食品部門の営業部に所属</dd>
+                            </dl>
+                            <dl class="history-area">
+                                <dt class="history-day">2007.4</dt>
+                                <dd class="history-detail">日配品の販売戦略部に異動</dd>
+                            </dl>
+                            <dl class="history-area">
+                                <dt class="history-day">2010.4</dt>
+                                <dd class="history-detail">惣菜部門営業主任に従事</dd>
+                            </dl>
+                            <dl class="history-area">
+                                <dt class="history-day">2012.3</dt>
+                                <dd class="history-detail">会社の惣菜部門長に就任</dd>
+                            </dl>
+                            <dl class="history-area">
+                                <dt class="history-day">2018.4</dt>
+                                <dd class="history-detail">株式会社おにぎりやを設立</dd>
+                            </dl>
+                            <dl class="history-area">
+                                <dt class="history-day">2023.5</dt>
+                                <dd class="history-detail">静岡県東部にも事業エリアを展開</dd>
+                            </dl>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 <?php get_footer(); ?>
