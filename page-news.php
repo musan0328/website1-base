b4f1dae907f8fe035bfce3eaffbeddf758e3427c
yamamura <ayatsujisan2@gmail.com>
2023-06-04 14:24:45 +0900

    コミットします

--- page-news.php
+++ page-news.php
@@ -21,6 +21,18 @@
                             </ul>
                      </div>
             </div>
+
+<?php
+//参考サイト https://ecco.co.jp/blog/wp_query/
+// 固定ページに特定のカテゴリーを表示
+ $args = array(
+    'post_type' => 'post',
+    'category_name' => 'news',
+    'posts_per_page' => 6
+ );
+ $the_query = new WP_Query( $args );
+ if ( $the_query->have_posts() ) :
+?>
 <div class="topNewsArea">
         <div class="NewsGroup">
             <div class="NewsArea">
@@ -29,12 +41,14 @@
             </div>
             <div class="NewsContent">
                 <div class="news">
-                        <a href="#" class="news-link">
+                        <a href="<?php echo get_permalink(); ?>" class="news-link">
                             <div class="news-area">
-                                    <p class="news-day">2023.05.01</dt>
-                                    <p class="news-title">新しいメニューができました。</dt>
+                                    <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+                                    <p class="news-day"><?php the_date(); ?></p>
+                                    <p class="news-title"><?php the_title(); ?></p>
                             </div>
                         </a>
+                        <?php endwhile; ?>
                 </div>
                 <div class="news">
                     <a href="#" class="news-link">
@@ -79,5 +93,6 @@
               </div>
             </div>
         </div>
-
+     <?php else: ?>
+   <?php endif; wp_reset_postdata(); ?>
 <?php get_footer(); ?>
